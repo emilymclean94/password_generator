e67e9b79505a46c12e45a415c730113ab270cbb7
Emily McLean <emilymclean93@gmail.com>
2023-03-29 16:42:00 -0400

    added character variables and psuedo code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+const symbol = ["!", "@", "$" ,"%", "^", "&", "*", "(", "(", "?", "+", "=", "-", ",", "~", ";", ":",]
+
+console.log(uppercase)
+console.log(lowercase)
+console.log(numbers)
+console.log(symbol)
 
 // Write password to the #password input
 function writePassword() {
