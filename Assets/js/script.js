48cc61d23b5fee28b88598cc7d6d0ba2465cec47
Emily McLean <emilymclean93@gmail.com>
2023-03-30 08:18:34 -0400

    added character arrays & length choice

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,17 @@
+var characterLength = 8;
+var choiceArray = [];
+
+
+const upArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+const lowArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const symArray = ["!", "@", "$", "%", "^", "&", "*", "(", "(", "?", "+", "=", "-", ",", "~", ";", ":",]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
-const symbol = ["!", "@", "$" ,"%", "^", "&", "*", "(", "(", "?", "+", "=", "-", ",", "~", ";", ":",]
 
-console.log(uppercase)
-console.log(lowercase)
-console.log(numbers)
-console.log(symbol)
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -16,8 +19,32 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  // generate password based on prompts
+}
+
+function getPrompts() {
+  characterLength = parseInt(window.prompt("Please choose a password between 8 and 128 characters"));
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    window.prompt("Character length invalid. Please choose a password between 8 and 128 characters.");
+    return false;
+  }
+
+  if (window.confirm("Would you like your password to contain uppercase letters?")) {
+    choiceArray = choiceArray.concat(upArray);
+  }
+  if (window.confirm("Would you like your password to contain lowercase letters?")) {
+    choiceArray = choiceArray.concat(lowArray);
+  }
+  if (window.confirm("Would you like your password to contain numbers?")) {
+    choiceArray = choiceArray.concat(numArray);
+  }
+  if (window.confirm("Would you like your password to contain special characters?")) {
+    choiceArray = choiceArray.concat(symArray);
+  }
+  return true;
+}
+
